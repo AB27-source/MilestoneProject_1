b7311b6bb4059515c42638aeaad3ce6eeb2e6f60
Angad <angadbhatti18@gmail.com>
2022-02-21 19:51:51 -0800

    Fixed Background to canvas, added rectangle object that jumps and moves

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 function createImage(source){
@@ -10,46 +10,80 @@ function createImage(source){
 const batman = createImage('./assets/Character/batman.png')
 const gothamCity = createImage('./assets/maps/gothamcity.jpg');
 
+// gothamCity.onload = function(){
+//     backgroundContext.drawImage(gothamCity,0,0);
+// }
 
-gothamCity.onload = function(){
-    context.drawImage(gothamCity,0,0);
-}
+const gravity = 0.1;
 
 class Player{
     constructor(){
+        // Position of character on canvas
         this.position = {
             x: 100,
             y: 340
         }
+        // Speed 
         this.speed = {
             x: 0,
-            y: 0
+            y: 2
         }
         this.width = 30;
         this.height = 30;
     }
     draw(){
-        context.fillStyle = 'red'; 
-        context.fillRect(this.position.x, this.position.y, this.width, this.height)
+        context.fillStyle = 'red'; // setting rectangle to color red
+        context.fillRect(this.position.x, this.position.y, this.width, this.height) //assiging constructor values to rectangle
     }
+    // updates player position
     update(){
-        this.draw()
-        this.position.y += this.speed.y;
-        this.position.x += this.speed.x;
+        this.draw() //calling draw function above
+        // Referenced from W3 Schools Game Gravity
+        this.position.x += this.speed.x;    //updating position.x, position.x = position.x + speed.x
+        this.position.y += this.speed.y;    //updating position.y, position.y = position.y + speed.y
         
+        // conditional that stops character at the bottom of the canvas
         if(this.position.y + this.height + this.speed.y  <= canvas.height)
             this.speed.y += gravity;
+
         else
             this.speed.y = 0;
+        
+
     }
 }
 
+// event listner for when user presses space; object goes up by 5
+addEventListener('keydown', ({key})=>{
+    if (key == ' '){
+        player.speed.y -= 5;            //subtracting player.speed.y by 5 and storing into player.spped.y
+    }
+})
+// event listner for when user lets go of space; object goes up by 1
+addEventListener('keyup', ({key})=>{
+    if (key == ' ')
+        player.speed.y -= 1;           //subtracting player.speed.y by 1 and storing into player.spped.y
+})
+
+
+//class for tubes
+class Tubes{
+    constructor(){
+        this.position = {
+            x: 0,
+            y: 0
+        }
+    }
+}
+
+// calling player class
 const player = new Player();
 
 function animate(){
-    requestAnimationFrame(animate);
-
-    player.update();
+    requestAnimationFrame(animate);     // recursively calling animate function
+    context.clearRect(0,0,canvas.width,canvas.height);   //clears past drawn objects on canvas
+    context.drawImage(gothamCity,0,0);         //adding background to canvas
+    player.speed.x = 0.3;   //starting speed of object
+    player.update();    //calling update function in class Player
 }
-
 animate();
